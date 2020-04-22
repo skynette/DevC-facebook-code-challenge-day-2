b1d4f047a6c007ea1a9f6deb709ab0f2862d3f59
L0n3w4lf <cutejosh2@gmail.com>
2020-04-22 18:53:53 +0100

    first attemot

--- day2.py
+++ day2.py
@@ -1,3 +1,23 @@
 
 # Complete the sockMerchant function below.
-def sockMerchant(n, ar):
+#  arr = [1, 2, 1, 2, 1, 3, 2]
+#  pair = {'1':1, '2': 1, '3':0}
+#  pair.values() = [1,1,0]
+#  sum = 1+1 = 2
+
+
+def sockMerchant(n, arr):
+	arr = []
+	for i in range(n):
+		sock = int(input())
+		arr.append(sock)
+	pair = {}
+	for i in arr:
+		occurence = (arr.count(i))//2
+		pair[i] = occurence
+	
+	pair = list(pair.values())
+	return sum(pair)
+
+
+
